1d2b4391fb27bbc37b271202a7610814b0d2eaf7
Avshalom Begler <avshalom210@gmail.com>
2026-05-25 10:40:52 +0300

    Add descriptive docstrings for S3 versioning tests

--- tests/test_s3_versioning.py
+++ tests/test_s3_versioning.py
@@ -1,10 +1,11 @@
-"""."""
+"""Tests for S3 versioning behavior: multiple versions and delete markers."""
 
 import pytest
 from botocore.exceptions import ClientError
 
 
 def test_put_object_twice_creates_two_versions(s3_client, versioned_bucket) -> None:
+    """Uploading the same key twice in a versioned bucket creates two distinct versions with independent content."""
     key = "versioning/test.txt"
 
     response_v1 = s3_client.put_object(Bucket=versioned_bucket, Key=key, Body=b"first")
@@ -23,6 +24,7 @@ def test_put_object_twice_creates_two_versions(s3_client, versioned_bucket) -> N
 
 
 def test_delete_in_versioned_bucket_creates_delete_marker(s3_client, versioned_bucket) -> None:
+    """Deleting an object in a versioned bucket inserts a delete marker instead of removing the version."""
     key = "versioning/test.txt"
     s3_client.put_object(Bucket=versioned_bucket, Key=key, Body=b"first")
     s3_client.delete_object(Bucket=versioned_bucket, Key=key)
