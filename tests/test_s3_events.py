8dd8eec623232c90067417c5e863c0f89686b3d5
Avshalom Begler <avshalom210@gmail.com>
2026-05-27 06:08:18 +0300

    Refactor S3 event notification test to use queue factory for temporary SQS queue

--- tests/test_s3_events.py
+++ tests/test_s3_events.py
@@ -6,9 +6,10 @@ from aws_qa_learning.helpers.s3 import configure_s3_to_sqs_notification
 from aws_qa_learning.helpers.sqs import get_queue_arn
 
 
-def test_s3_put_triggers_sqs_notification(s3_client, sqs_client, temporary_bucket, temporary_queue) -> None:
+def test_s3_put_triggers_sqs_notification(s3_client, sqs_client, temporary_bucket, queue_factory) -> None:
     """Verify that uploading an object to S3 triggers an SQS notification with correct metadata."""
-    queue_arn = get_queue_arn(sqs_client, temporary_queue)
+    queue_url = queue_factory()
+    queue_arn = get_queue_arn(sqs_client, queue_url)
 
     configure_s3_to_sqs_notification(s3_client, temporary_bucket, queue_arn, ["s3:ObjectCreated:*"])
 
@@ -17,14 +18,14 @@ def test_s3_put_triggers_sqs_notification(s3_client, sqs_client, temporary_bucke
     s3_client.put_object(Bucket=temporary_bucket, Key=key, Body=body)
 
     response = sqs_client.receive_message(
-        QueueUrl=temporary_queue,
+        QueueUrl=queue_url,
         MaxNumberOfMessages=10,
         WaitTimeSeconds=15,
     )
 
     messages = response.get("Messages", [])
 
-    # Fitlering LocalStack test notification
+    # Filtering LocalStack test notification
     s3_events = [msg for msg in messages if "Records" in json.loads(msg["Body"])]
 
     assert len(s3_events) == 1
