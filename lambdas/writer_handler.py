502e111b7b0881c4aa49abe0a86a3af5f0b30060
Avshalom Begler <avshalom210@gmail.com>
2026-06-15 08:52:06 +0300

    Add Lambda handler to write items to DynamoDB table

---
+++ lambdas/writer_handler.py
@@ -0,0 +1,22 @@
+"""Lambda handler that writes an item to a DynamoDB table."""
+
+import os
+
+import boto3
+
+
+def handler(event, context):
+    """Put the item from the event into the specified DynamoDB table."""
+    # print(f'Received event: {event}')
+
+    endpoint_url = os.environ.get('AWS_ENDPOINT_URL')
+    # print(f'AWS_ENDPOINT_URL: {endpoint_url}')
+
+    dynamodb_client = boto3.client('dynamodb', endpoint_url=endpoint_url)
+    response = dynamodb_client.put_item(
+        TableName=event['table_name'],
+        Item=event['item'],
+    )
+    # print(f'put_item HTTP status: {response["ResponseMetadata"]["HTTPStatusCode"]}')
+
+    return {'put_status_code': response['ResponseMetadata']['HTTPStatusCode']}
