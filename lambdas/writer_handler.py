eb2fb10c12cf1d009dcbdbaa88a095f541577e27
Avshalom Begler <avshalom210@gmail.com>
2026-06-21 15:02:11 +0300

    Remove commented debug print statements from writer_handler

--- lambdas/writer_handler.py
+++ lambdas/writer_handler.py
@@ -13,16 +13,12 @@ def handler(event, context):
     ``event['item']`` (the item in DynamoDB's attribute-value format).
     Returns a dict with the ``put_item`` call's HTTP status code.
     """
-    # print(f'Received event: {event}')
-
     endpoint_url = os.environ.get('AWS_ENDPOINT_URL')
-    # print(f'AWS_ENDPOINT_URL: {endpoint_url}')
 
     dynamodb_client = boto3.client('dynamodb', endpoint_url=endpoint_url)
     response = dynamodb_client.put_item(
         TableName=event['table_name'],
         Item=event['item'],
     )
-    # print(f'put_item HTTP status: {response["ResponseMetadata"]["HTTPStatusCode"]}')
 
     return {'put_status_code': response['ResponseMetadata']['HTTPStatusCode']}
